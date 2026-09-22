fa17d1ce723430a78597f6b3fafeec3ee36db0e7
adadvar <alirezadadvar24@gmail.com>
2026-09-22 13:22:40 +0330

    ch .sth

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,45 +1,42 @@
-import { notoNastaliq } from "@/app/layout";
-import Image from "next/image";
-import Link from "next/link";
-import { BsSearch } from "react-icons/bs";
+import { notoNastaliq } from '@/app/layout';
+import Image from 'next/image';
+import Link from 'next/link';
+import { BsSearch } from 'react-icons/bs';
 const Navbar = ({ className }: { className: string }) => {
   const navItemClass =
-    "border-b-2 py-3 border-b-transparent hover:border-[#E5E7E8] transition-colors";
+    'border-b-2 py-3 border-b-transparent hover:border-[#E5E7E8] transition-colors';
   return (
     <header
-      className={`${className} px-[3rem] py-[2rem] flex items-center justify-between text-[#E5E7E8]`}
-    >
-      <Link href={"/"} className="flex gap-2 items-center">
+      className={`${className} flex items-center justify-between px-[3rem] py-[2rem] text-[#E5E7E8]`}>
+      <Link href={'/'} className="flex items-center gap-2">
         <Image
-          src={"/apple-touch-icon.png"}
+          src={'/apple-touch-icon.png'}
           alt="لوگو امام مهدی"
           width={50}
           height={50}
           priority
           quality={50}
         />
-        <span className={`text-[2.4rem] ${notoNastaliq.className}`}>
-          مهدی موعود
-        </span>
+        <span className={`mb-6 text-[2.4rem] ${notoNastaliq.className}`}>مهدی موعود</span>
       </Link>
       <nav className="">
-        <ul className="flex items-center text-[1.6rem] justify-between gap-[3.2rem]">
+        <ul className="flex items-center justify-between gap-[3.2rem] text-[1.6rem]">
           <li className={navItemClass}>
-            <Link href={"#"}>خانه</Link>
+            <Link href={'#'}>خانه</Link>
           </li>
           <li className={navItemClass}>
-            <Link href={"#"}>مطالب</Link>
+            <Link href={'#'}>مطالب</Link>
           </li>
           <li className={navItemClass}>
-            <Link href={"#"}>درباره امام زمان</Link>
+            <Link href={'#'}>درباره امام زمان</Link>
           </li>
           <li className={navItemClass}>
-            <Link href={"#"}>درباره این پروژه</Link>
+            <Link href={'#'}>درباره این پروژه</Link>
           </li>
         </ul>
       </nav>
       <button className="">
-        <BsSearch className="w-[2.4rem] h-[2.4rem]" />
+        <BsSearch className="h-[2.4rem] w-[2.4rem]" />
       </button>
     </header>
   );
